cd920e437f6c581f51e6918538db8f0bd01ecb06
dl364823 <146682379+dl364823@users.noreply.github.com>
2024-09-27 20:09:43 -0400

    Complete a simple Login page and apply Tailwind CSS styling to it.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Login from './Login';
 
-function App() {
+
+const App = () => {
+  
   return (
       <Router>
         <div className="App">
@@ -11,12 +14,17 @@ function App() {
             <ul className="menu menu-horizontal px-1">
               <li><Link to="/">Home</Link></li>
               <li><Link to="/about">About</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/payment">Payment</Link></li>
+              
+
             </ul>
           </nav>
 
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </Router>
