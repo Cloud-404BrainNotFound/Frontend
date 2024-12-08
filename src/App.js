4255420c969e1f2d9986737ce2d3823dd2111f3c
dl364823 <146682379+dl364823@users.noreply.github.com>
2024-12-07 20:58:04 -0500

    merge landingpage

--- src/App.js
+++ src/App.js
@@ -24,17 +24,14 @@ const App = () => {
           <nav className="bg-base-100">
             <ul className="menu menu-horizontal px-1">
               <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/payment">Payment</Link></li>
-              <li><Link to="/stringing-order">Stringing Order</Link></li>
+
             </ul>
           </nav>
 
           <Routes>
             <Route path="/login" element={<Login />} /> 
             <Route path="/signup" element={<Signup />} />
-            <Route path="/landing" element={<LandingPage />} />
+            <Route path="/" element={<LandingPage />} />
             <Route 
               path="/home" 
               element={
